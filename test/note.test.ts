bc7ddff24aa616efc3ebcc8502e5ad950bef751e
Travis Vachon <travis.vachon@gmail.com>
2022-06-27 16:01:31 -0700

    organize/export functions
    developing from the testfile made sense for a while, but this is much better

--- test/note.test.ts
+++ test/note.test.ts
@@ -1,131 +1,16 @@
 import {
-  SolidDataset, Thing,
-  getThing, buildThing, createThing, setThing,
-  createSolidDataset,
-  addUrl,
-  setInteger, setDecimal, setStringNoLocale, setBoolean,
-  getStringNoLocale, getInteger, getBoolean, getUrl,
-  saveSolidDatasetAt,
+  Thing,
+  getThing, setThing, createSolidDataset, saveSolidDatasetAt
 } from '@inrupt/solid-client';
-import { createNS } from "@ontologies/core"
-import { first, rest, nil } from '@ontologies/rdf'
+
+import { arrayToThings, thingsToArray } from '../src/collections'
+import { createThingFromSlateJSOElement, noteThingToSlateObject } from '../src/note'
 
 const noteBody = [{ "children": [{ "text": "Hi everybody!" }], "type": "h1" }, { "type": "p", "id": 1651696062634, "children": [{ "text": "" }] }, { "children": [{ "text": "" }], "type": "h1" }, { "children": [{ "text": "thanks", "bold": true }, { "text": " for coming to the " }, { "text": "digital gardening party", "italic": true }, { "text": "." }], "type": "h2" }, { "children": [{ "text": "" }], "type": "h2" }, { "children": [{ "text": "I'm " }, { "type": "concept", "children": [{ "text": "[[Travis]]" }], "name": "Travis" }, { "text": "." }], "type": "h2" }, { "children": [{ "text": "" }], "type": "h2" }, { "children": [{ "text": "This is not a cat:" }], "type": "h2" }, { "children": [{ "text": "" }], "type": "h2" }, { "children": [{ "text": "" }], "type": "h2" }, { "type": "img", "children": [{ "text": "" }], "url": "https://travis.mysilio.me/public/itme/online/images/1cd18da0-6c9e-11eb-b09e-dd1bdda70e9f.jpg", "originalUrl": "https://travis.mysilio.me/public/itme/online/images/1cd18da0-6c9e-11eb-b09e-dd1bdda70e9f.original.jpg", "alt": "", "mime": "image/jpeg" }]
-const noteNSUrl = "https://mysilio.garden/ontologies/note#"
-const noteNS = createNS(noteNSUrl)
-
-function addKeyValToThing(thing: Thing, key: string, value: any, path: number[]): Thing[] {
-  if (Array.isArray(value)) {
-    const [arrayThing, ...restOfThings] = slateArrayToThings(value, [...path, 0])
-    if (arrayThing) {
-      return [
-        addUrl(thing, key, arrayThing),
-        arrayThing,
-        ...restOfThings
-      ]
-    } else {
-      // TODO: is this the right thing to do? arrayThing should never be falsy so maybe throw an error?
-      return []
-    }
-  } else {
-
-    switch (typeof value) {
-      case 'string':
-        return [setStringNoLocale(thing, key, value)]
-      case 'boolean':
-        return [setBoolean(thing, key, value)]
-      case 'number':
-        if (Number.isInteger(value)) {
-          return [setInteger(thing, key, value)]
-        } else {
-          return [setDecimal(thing, key, value)]
-        }
-      default:
-        return [thing]
-    }
-  }
-
-}
-
-function createThingFromSlateJSOElement(o: object, path: number[]): Thing[] {
-  const thing = createThing({ name: ((o as any).id ? `${(o as any).id}` : `el-${path.join("-")}`) })
-  const otherThings = Object.keys(o).reduce((m: Thing[], k: string) => {
-    const [mThing, ...mRest] = m;
-    const [thing, ...restOfThings] = addKeyValToThing(mThing, noteNS(k).value, (o as any)[k], path)
-    return [thing, ...mRest, ...restOfThings]
-  }, [thing])
-  return [thing, ...otherThings]
-}
-
-function slateArrayToThings(slateArray: object[], path: number[] = [0]): Thing[] {
-  const [el, ...restOfEls] = slateArray
-  const last = path[path.length - 1]
-  const [listElementThing, ...listElementSubThings] = createThingFromSlateJSOElement(el, path)
-  const [nextListThing, ...nextListSubThings] = (restOfEls.length > 0) ? slateArrayToThings(restOfEls, [...path.slice(0, path.length - 1), last + 1]) : []
-  const listThing = buildThing(createThing({ name: `li-${path.join("-")}` }))
-    .addUrl(first.value, listElementThing)
-    .addUrl(rest.value, nextListThing || nil.value)
-    .build()
-
-  return [listThing, listElementThing, ...listElementSubThings, nextListThing, ...nextListSubThings]
-}
-
-function childrenArrayFromDataset(dataset: SolidDataset, childrenUrl: string) {
-  const thing = getThing(dataset, childrenUrl);
-
-  if (thing) {
-    return thingsToSlateArray(thing, dataset)
-  } else {
-    return []
-  }
-}
-
-const childrenPred = noteNS('children')
-function thingToSlateObject(thing: Thing, dataset: SolidDataset) {
-  const obj: any = {}
-  for (const pred in thing.predicates) {
-    const [_, key] = pred.split(noteNSUrl)
-    if (key) {
-      if (key === 'children') {
-        const children = getUrl(thing, childrenPred.value)
-        if (children) obj.children = childrenArrayFromDataset(dataset, children)
-      } else {
-        const literals = thing.predicates[pred].literals
-        if (literals) {
-          if (literals['http://www.w3.org/2001/XMLSchema#string']) {
-            const str = getStringNoLocale(thing, pred)
-            if (str || (str === "")) obj[key] = str
-          } else if (literals['http://www.w3.org/2001/XMLSchema#boolean']){
-            const bool = getBoolean(thing, pred)
-            if (bool === true || bool === false) obj[key] = bool
-          } else if (literals['http://www.w3.org/2001/XMLSchema#integer']){
-            const n = getInteger(thing, pred)
-            if (n) obj[key] = n
-          }
-        }
-      }
-    }
-  }
-
-  return obj
-}
-
-function thingsToSlateArray(thing: Thing, dataset: SolidDataset): object[] {
-  const restValue = getUrl(thing, rest.value)
-  const restThing = (restValue && (restValue !== nil.value)) && getThing(dataset, restValue)
-  const firstThing = getThing(dataset, getUrl(thing, first.value))
-  const firstElement = firstThing && thingToSlateObject(firstThing, dataset)
-  return firstElement ? [
-    firstElement,
-    ...(restThing ? thingsToSlateArray(restThing, dataset) : [])
-  ] : []
-}
-
-
 
 describe('a JSON note body', () => {
   it('can be serialized and deserialized to and from RDF', async () => {
-    const things: Thing[] = slateArrayToThings(noteBody)
+    const things: Thing[] = arrayToThings(noteBody, createThingFromSlateJSOElement)
     const listUrl = things[0].url
 
     let dataset = things.filter(x => !!x).reduce(setThing, createSolidDataset())
@@ -142,7 +27,7 @@ describe('a JSON note body', () => {
     })
 
     const listThing = getThing(dataset, listUrl)
-    const newNoteBody = listThing && thingsToSlateArray(listThing, dataset)
+    const newNoteBody = listThing && thingsToArray(listThing, dataset, noteThingToSlateObject)
     expect(newNoteBody).toEqual(noteBody);
   });
 });
