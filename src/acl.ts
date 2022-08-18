ec20b419a766fa49fb338c59e09a6560214e43ac
Travis Vachon <travis.vachon@gmail.com>
2022-08-17 20:49:02 -0700

    add method for setting resource permissions based on a garden
    eventually this should look at the garden's acl and set the resources to match, but for now just base it on the garden's name.
    
    once we introduce slugs to gardens we should use the slug instead

--- src/acl.ts
+++ src/acl.ts
@@ -1,15 +1,38 @@
 
 import {
   getResourceAcl,
-  setPublicResourceAccess,
-  UrlString,
   saveAclFor,
   hasAccessibleAcl,
   createAclFromFallbackAcl,
   hasFallbackAcl,
-  getFileWithAcl
-} from '@inrupt/solid-client';
+  getFileWithAcl,
+} from '@inrupt/solid-client/acl/acl';
+import { setPublicResourceAccess } from '@inrupt/solid-client/acl/class';
+import { Thing, UrlString } from '@inrupt/solid-client/interfaces'
+import { getSourceUrl } from '@inrupt/solid-client/resource/resource'
+import { getThing } from '@inrupt/solid-client/thing/thing'
 
+import { getTitle } from './utils'
+import { Garden } from './types'
+
+export async function setPublicAccessBasedOnGarden(urls: UrlString[], garden: Garden, options: any) {
+  // set perms based on garden name for now. custom gardens with custom permissions
+  // will require more sophisticated access checking - the universal access
+  // api may be enough for this but doesn't seem to be work
+  const gardenUrl = getSourceUrl(garden)
+  if (gardenUrl) {
+    const publicRead = (getTitle(getThing(garden, gardenUrl) as Thing) === 'Public') ? true : false
+    return Promise.all(
+      urls.map(url =>
+        setPublicAccess(url,
+          { read: publicRead, append: false, write: false, control: false },
+          options)
+      )
+    )
+  } else {
+    throw new Error("cannot set public access for a garden without a source url")
+  }
+}
 
 export async function setPublicAccess(resourceUrl: UrlString, access: any, options: any) {
   const resource = await getFileWithAcl(resourceUrl, options)
