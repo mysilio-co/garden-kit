cc16db0b7064ada07b0ae1b03f587290ce17d46d
Ian Davis <jungziege@gmail.com>
2023-02-14 15:39:35 +0000

    Add hasFuseIndex predicate

--- src/vocab.ts
+++ src/vocab.ts
@@ -60,6 +60,8 @@ export const MY = {
     subscribedTo: g`subscribedTo`, // a property of a Webhookd that indicates which Resource URL the Webhook is currently subscribed to.
     deliversTo: g`deliversTo`, // a property of a Webhook that indicates what URL the Webhook delivers notifications to.
     unsubscribeWith: g`unsubscribeWith`, // a property of a Webhook that indicates what URL to unsubscribe from this webhook with.
+
+    hasFuseIndex: g`hasFuseIndex`, // a link to a saved json index for use with Fuse.js
   },
 };
 
